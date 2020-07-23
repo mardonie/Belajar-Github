8e4fe81361f845a3fd72f5fb88a63bb4fa3bb748
mardoni <mardoni_sun@yahoo.com>
2020-07-23 11:05:19 +0700

    menambahkan clone ke pada github

--- home.php
+++ home.php
@@ -2,5 +2,5 @@
 1. Buat Folder Latihan Github
 2. Buat File
 3. Buat Repository dengan Codeigniter dan license GNU dengan private
-
+4. Pada git bash Ketik git clone https://github.com/mardonie/Belajar-Github.git
 */
